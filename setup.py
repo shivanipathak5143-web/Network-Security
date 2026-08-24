771c11d69e2054f5f54f8267d645d6e64d474fb2
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-24 16:54:09 +0530

    Setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    '''
+    This function will return list of requirements
+    '''
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!= '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+    return requirement_list
+
+setup(
+    name='NetworkSecurity',
+    version='0.0.1',
+    author='Shivani',
+    author_email='shivanipathak5143@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
