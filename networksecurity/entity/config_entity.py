9972a2be1effeccd281ae81ddc00a308a58ae1f3
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-25 19:47:56 +0530

    Data Ingestion

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -5,7 +5,7 @@ from networksecurity.constant import training_pipeline
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
-class TrainigPipelineConfig:
+class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         timestamp=timestamp.strftime('%m_%d_%Y_%H_%M_%S')
         self.pipeline_name=training_pipeline.PIPELINE_NAME
