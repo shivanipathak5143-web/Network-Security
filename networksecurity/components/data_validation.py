bdd100845543ace12e2ca81d5fa23fccebea337c
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-28 11:26:40 +0530

    Data Validatoon

---
+++ networksecurity/components/data_validation.py
@@ -0,0 +1,12 @@
+from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from scipy.stats import ks_2samp
+import pandas as pd
+import os,sys
+
+
+
+
+
