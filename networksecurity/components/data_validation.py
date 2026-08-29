d768a2e5ce451e16a2dae93a257d1035d8a35ac0
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-29 10:31:19 +0530

    Data Validation

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -18,6 +18,49 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def validate_number_of_columns(self,dataframe:pd.DataFrame)->bool:
+        try:
+            number_of_columns=len(self._schema_config)
+            logging.info(f"Required number of columns: {number_of_columns}")
+            logging.info(f"Data frame has columns: {len(dataframe.columns)}")
+            if len(dataframe.columns)==number_of_columns:
+                return True
+            return False
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try:
+            train_file_path=self.data_ingestion_artifact.trained_file_path
+            test_file_path=self.data_ingestion_artifact.test_file_path
+
+            train_dataframe=DataValidation.read_data(train_file_path)
+            test_dataframe=DataValidation.read_data(test_file_path)
+
+            status=self.validate_number_of_columns(dataframe=train_dataframe)
+            if not status:
+                error_message=f"{error_message} Train dataframe does not contain all columns. \n"
+            status=self.validate_number_of_columns(dataframe=test_dataframe)
+            if not status:
+                error_message=f"{error_message} Test dataframe does not contain all columns. \n"
+
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
 
 
 
