08564b7c71cb53c08964a6d7058b5c6c008fc7ee
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-29 10:40:38 +0530

    Setting Utils.py

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -38,7 +38,25 @@ class DataValidation:
 
     def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
         try:
-            pass
+            status=True
+            report={}
+            for column in base_df.columns:
+                d1=base_df[column]
+                d2=current_df[column]
+                is_same_dist=ks_2samp(d1,d2)
+                if threshold<=is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found=True
+                    status=False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                }})
+            drift_report_file_path=self.data_validation_config.drift_report_file_path
+            dir_path=os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
