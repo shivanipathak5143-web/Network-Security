c7cd73987c5109ff7835643c1916704debf8671b
Shivani Pathak <shivanipathak5143@gmail.com>
2026-08-31 12:04:48 +0530

    Data Transformation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -23,3 +23,12 @@ def write_yaml_file(file_path:str, content:object, replace:bool=False)->None:
             yaml.dump(content,file)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+
+def save_numpy_array_data(file_path: str, array:np.array):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
