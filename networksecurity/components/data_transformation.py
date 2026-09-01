a8a401adfe1e0c19d3a0fcdd2709f1e165e388ae
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-01 16:10:16 +0530

    Data Transformation

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -14,3 +14,25 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.utils.main_utils.utils import save_numpy_array_data, save_object
 
+class DataTransformation:
+    def __init__(self,data_validation_artifact:DataValidationArtifact,
+                 data_transformation_config:DataTransformationConfig):
+            try:
+                  self.data_validation_artifact:DataValidationArtifact=data_validation_artifact
+                  self.data_transformation_config:DataTransformationConfig=data_transformation_config
+            except Exception as e:
+                  raise NetworkSecurityException(e,sys)
+    @staticmethod
+    def read_data(file_path)->pd.DataFrame:
+          try:
+                return pd.read_csv(file_path)
+          except Exception as e:
+                raise NetworkSecurityException(e,sys)
+
+    def initiate_data_transformation(self)->DataValidationArtifact:
+          logging.info('Entered initiate data_transformation method od DataTransformation class')
+          try:
+                logging.info('Starting data transformation')
+          except Exception as e:
+                raise NetworkSecurityException(e,sys)
+
