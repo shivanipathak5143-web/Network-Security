26f55c85c728bf40e7f5c0121f89c55290ca4ed8
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-01 16:30:45 +0530

    Data Transformation changes

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -29,10 +29,42 @@ class DataTransformation:
           except Exception as e:
                 raise NetworkSecurityException(e,sys)
 
+    def get_data_transformer_object(cls)->Pipeline:
+          logging.info('Entered get_data_transformer_object method of Transformation class')
+          try:
+                imputer:KNNImputer=KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS)
+                logging.info(f"Initialise KNNImputer with {DATA_TRANSFORMATION_IMPUTER_PARAMS}")
+                processor:Pipeline=Pipeline([("imputer",imputer)])
+                return processor
+          except Exception as e:
+                raise NetworkSecurityException(e,sys)
+
     def initiate_data_transformation(self)->DataValidationArtifact:
           logging.info('Entered initiate data_transformation method od DataTransformation class')
           try:
                 logging.info('Starting data transformation')
+                train_df=DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+                test_df=DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+                input_feature_train_df=train_df.drop(columns=[TARGET_COLUMN],axis=1)
+                target_feature_train_df=train_df[TARGET_COLUMN]
+                target_feature_train_df=target_feature_train_df.replace(-1,0)
+
+                input_feature_test_df=test_df.drop(columns=[TARGET_COLUMN],axis=1)
+                target_feature_test_df=test_df[TARGET_COLUMN]
+                target_feature_test_df=target_feature_test_df.replace(-1,0)
+
+                preprocessor=self.get_data_transformer_object()
+                preprocessor_object=preprocessor.fit(input_feature_train_df)
+                transformed_input_train_feature=preprocessor_object.transform(input_feature_train_df)
+                transformed_input_test_feature=preprocessor_object.transform(input_feature_test_df)
+
+                train_arr=np.c_[transformed_input_train_feature,np.array(target_feature_train_df)]
+                test_arr=np.c_[transformed_input_test_feature, np.arrary(target_feature_test_df)]
+
+                save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr, )
+                save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr, )
+                save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
           except Exception as e:
                 raise NetworkSecurityException(e,sys)
 
