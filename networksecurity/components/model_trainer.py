6e5be48f89dc825b977895f53c6f017a0309870e
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-02 19:26:30 +0530

    Changes in Model Trainer

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -6,7 +6,7 @@ from networksecurity.entity.artifact_entity import DataTransformationArtifact, M
 from networksecurity.entity.config_entity import ModelTrainerConfig
 from networksecurity.utils.main_utils.utils import save_object, load_object
 from networksecurity.utils.main_utils.utils import load_numpy_array_data
-from networksecurity.utils.ml_utils.classification_metric import get_classification_score
+from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
@@ -18,8 +18,26 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
+    def train_model(self,x_train,y_train):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
     def initiate_model_trainer(self)->ModelTrainerArtifact:
         try:
-            
+            train_file_path=self.data_transformation_artifact.transformed_train_file_path
+            test_file_path=self.data_transformation_artifact.transformed_test_file_path
+
+            train_arr=load_numpy_array_data(train_file_path)
+            test_arr=load_numpy_array_data(test_file_path)
+
+            x_train,y_train,x_test,y_test=(
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1],
+            )
+            model=self.train_model(x_train,y_train)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
