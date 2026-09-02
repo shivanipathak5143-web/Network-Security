295e3396ba8f0e916d9b8e35af49c5e8d79f45a0
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-02 19:21:17 +0530

    Model Trainer

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from networksecurity.exception.exception import NetworkException
+from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
 from networksecurity.entity.config_entity import ModelTrainerConfig
@@ -10,3 +10,16 @@ from networksecurity.utils.ml_utils.classification_metric import get_classificat
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
+class ModelTrainer:
+    def __init__(self,model_train_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            self.model_trainer_config=self.model_trainer_config
+            self.data_transformation_artifact=data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def initiate_model_trainer(self)->ModelTrainerArtifact:
+        try:
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
