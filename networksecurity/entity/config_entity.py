a61d0922b401252b4440c81d94e6d120e228c2de
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-03 11:49:27 +0530

    Entity Changes

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -61,7 +61,7 @@ class DataTransformationConfig:
 class ModelTrainerConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.model_trainer_dir: str = os.path.join(
-            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINING_DIR_NAME
         )
         self.trained_model_file_path: str = os.path.join(
             self.model_trainer_dir, training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
