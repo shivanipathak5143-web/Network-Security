71cdb190ccc8ccb93f46fa278a551c09739a428e
Shivani Pathak <shivanipathak5143@gmail.com>
2026-09-07 09:30:03 +0530

    App.py changes

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
-    allow_header=['*']
+    allow_headers=['*']
 )
 
 @app.get('/',tags=['authentication'])
